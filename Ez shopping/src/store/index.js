4df7e0b816e4e8ca81fcf7fb75b30ace73813c55
mineyas <mineyaskha@gmail.com>
2023-10-03 10:00:04 +0100

    added cart to store

--- Ez shopping/src/store/index.js
+++ Ez shopping/src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './slices/userSlice.js';
 import productsSlice from './slices/productsSlice.js';
+import cartSlice from './slices/cartSlice.js';
 
 const store = configureStore({
   reducer: {
     user: userSlice.reducer,
-    products: productsSlice.reducer
+    products: productsSlice.reducer,
+    cart: cartSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
