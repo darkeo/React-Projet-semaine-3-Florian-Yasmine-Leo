34abba95ec292a238a2024b808b153b3a83905a7
floriansaliba <florian.saliba@gmail.com>
2023-10-04 16:19:16 +0200

    ajout du middleware pour gérer le localStorage

--- Ez shopping/src/store/middleWare/saveBasketMiddleware.js
+++ Ez shopping/src/store/middleWare/saveBasketMiddleware.js
@@ -1,17 +1,18 @@
-import {
-  addToLocalStorage,
-  addToLocalStorageFromMiddleware,
-  isKeyInLocalStorage,
-} from '../../utils/localStorage';
+import { addToLocalStorage } from '../../utils/localStorage';
 import cartSlice from '../slices/cartSlice';
 
 const saveBasketMiddleware = (store) => (next) => (action) => {
   const nextAction = next(action);
-  //   if (action.type === cartSlice.actions.addItemToCart.toString()) {
-  //     const productId = action.payload;
-
-  //     addToLocalStorageFromMiddleware('cart', productId);
-  //   }
+  const cart = store.getState().cart.cart;
+  if (
+    action.type === cartSlice.actions.addItemToCart.toString() ||
+    action.type === cartSlice.actions.decreaseQuantity.toString() ||
+    action.type === cartSlice.actions.increaseQuantity.toString() ||
+    action.type === cartSlice.actions.removeItem.toString()
+  ) {
+    console.log(cart);
+    addToLocalStorage('cart', cart);
+  }
 
   return nextAction;
 };
