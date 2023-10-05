2bed939d5ed5726b37b55a6d13e077ec34b57854
floriansaliba <florian.saliba@gmail.com>
2023-10-05 14:49:29 +0200

    Afficher le prénom du user dans le panier

--- Ez shopping/src/pages/Cart/Cart.jsx
+++ Ez shopping/src/pages/Cart/Cart.jsx
@@ -3,17 +3,24 @@ import CartList from '../../component/CartList/CartList';
 import './Cart.scss';
 import { removeAllItems } from '../../store/slices/cartSlice';
 import { selectCart } from '../../store/selectors/cartSelectors';
+import { selectUser } from '../../store/selectors/userSelectors';
 
 export default function Cart() {
   const dispatch = useDispatch();
   const cart = useSelector(selectCart);
+  const user = useSelector(selectUser);
+  console.log(user);
 
   const handleClick = () => {
     dispatch(removeAllItems());
   };
   return (
     <div style={{ minHeight: '100vh' }}>
-      {cart.length > 0 ? <h1>Votre Panier</h1> : <h1>Votre Panier est vide</h1>}
+      {cart.length > 0 ? (
+        <h1>{user.firstName}, voici votre panier</h1>
+      ) : (
+        <h1>{user.firstName}, Votre Panier est vide</h1>
+      )}
       {cart.length > 0 && (
         <button
           onClick={() => {
