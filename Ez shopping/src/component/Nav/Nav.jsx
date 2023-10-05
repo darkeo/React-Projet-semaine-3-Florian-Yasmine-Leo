003d6777fde7a5b25a64b00879fc2a91f25884c1
mineyas <mineyaskha@gmail.com>
2023-10-05 15:11:40 +0100

    restyled darkmode

--- Ez shopping/src/component/Nav/Nav.jsx
+++ Ez shopping/src/component/Nav/Nav.jsx
@@ -1,34 +1,38 @@
-import { NavLink } from 'react-router-dom';
-import './Nav.scss';
+import { NavLink } from "react-router-dom";
+import "./Nav.scss";
 
 import {
   getFromLocalStorage,
   isKeyInLocalStorage,
-} from '../../utils/localStorage';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectUser } from '../../store/selectors/userSelectors';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { switchMode } from '../../store/slices/darkModeSlice';
+} from "../../utils/localStorage";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "../../store/selectors/userSelectors";
+import { useEffect } from "react";
+import { useState } from "react";
+import { switchMode } from "../../store/slices/darkModeSlice";
+
+import { BiSolidMoon } from "react-icons/bi";
+import { BsSunFill } from "react-icons/bs";
 
 export default function Nav() {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
-  const [firstName, setFirstName] = useState('Anonyme');
+  const [firstName, setFirstName] = useState("Anonyme");
+  const [isDarkMode, setIsDarkMode] = useState(false); // State for dark mode
 
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
   const style = ({ isActive }) => {
     return {
-      color: isActive ? 'white' : 'gray',
+      color: isActive ? "white" : "gray",
       //   backgroundColor: isActive ? "white" : "rgb(31, 101, 115)",
     };
   };
 
   useEffect(() => {
-    if (isKeyInLocalStorage('user')) {
-      const firstName = getFromLocalStorage('user').firstName;
+    if (isKeyInLocalStorage("user")) {
+      const firstName = getFromLocalStorage("user").firstName;
       firstName
         ? setFirstName(capitalizeFirstLetter(firstName))
         : setFirstName(capitalizeFirstLetter(user.firstName));
@@ -37,27 +41,30 @@ export default function Nav() {
 
   const toggleDarkMode = () => {
     dispatch(switchMode());
+    setIsDarkMode(!isDarkMode); // Toggle the dark mode state
   };
 
   return (
-    <nav className='nav-menu'>
-      <NavLink className='home-link' to={'/'}>
+    <nav className="nav-menu">
+      <NavLink className="home-link" to={"/"}>
         EZ Shopping
       </NavLink>
       <div>
-        <NavLink style={style} to={'/login'}>
+        <NavLink style={style} to={"/login"}>
           {firstName}
         </NavLink>
-        <NavLink style={style} to={'/cart'}>
+        <NavLink style={style} to={"/cart"}>
           Panier
         </NavLink>
-        <button
+        <div
           onClick={() => {
             toggleDarkMode();
           }}
+          className="button-darkmode"
         >
-          darkmode
-        </button>
+          {isDarkMode ? <BsSunFill size={30} /> : <BiSolidMoon size={30} />}{" "}
+          {/* Dark and light mode icons */}
+        </div>
       </div>
     </nav>
   );
