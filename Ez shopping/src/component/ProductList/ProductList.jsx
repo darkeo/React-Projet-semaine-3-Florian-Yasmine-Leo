e052cfcb9bf7c73fa28f7785f06e56a412dc34c1
mineyas <mineyaskha@gmail.com>
2023-10-05 10:57:32 +0100

    imported composant in product list

--- Ez shopping/src/component/ProductList/ProductList.jsx
+++ Ez shopping/src/component/ProductList/ProductList.jsx
@@ -2,6 +2,7 @@ import "./ProductList.scss";
 import { useDispatch } from "react-redux";
 import { addItemToCart } from "../../store/slices/cartSlice";
 import { updateProduct } from "../../store/slices/productsSlice";
+import Product from "./Product";
 
 const ProductList = ({ products }) => {
   const dispatch = useDispatch();
@@ -16,43 +17,20 @@ const ProductList = ({ products }) => {
   };
   return (
     <div>
-      <h2>Liste des Produits</h2>
       <div className="product-list">
         {products.map((product) => (
-          <div key={product.id} className="product-item">
-            <div className="product-box-category">
-            <p className="product-category">{product.category}</p>
-            </div>
-            <div className="picture-view">
-              <img src={product.image} alt="" className="product-image" />
-            </div>
-            <div className="product-info">
-              <h3 className="product-title">{product.title}</h3>
-              <p className="product-description">{product.description}</p>
-              <h4 className="product-price">${product.price}</h4>
-              <div className="product-quantity">
-                <p className="product-quantity-text">Quantity</p>
-                <input
-                  type="number"
-                  id={`product-quantity-box-${product.id}`}
-                  className="product-quantity-input"
-                  name="quantity"
-                  min="1"
-                  max="10"
-                  //value={quantity}
-                  onChange={(e) => {
-                    handleQuantityChange(e, product.id);
-                  }}
-                />
-              </div>
-              <button
-                onClick={() => handleAddToCart(product)}
-                className="product-add-to-cart"
-              >
-                Add to basket
-              </button>
-            </div>
-          </div>
+          <Product
+            key={product.id}
+            category={product.category}
+            image={product.image}
+            title={product.title}
+            description={product.description}
+            price={product.price}
+            handleQuantityChange={(e) => {
+              handleQuantityChange(e, product.id);
+            }}
+            handleAddToCart={() => handleAddToCart(product)}
+          />
         ))}
       </div>
     </div>
