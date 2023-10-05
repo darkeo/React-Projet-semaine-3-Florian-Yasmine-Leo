c197d234d6fc2641269596c9a0e806f72844e32c
floriansaliba <florian.saliba@gmail.com>
2023-10-05 10:40:19 +0200

    test darkMode

--- Ez shopping/src/App.jsx
+++ Ez shopping/src/App.jsx
@@ -1,21 +1,35 @@
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import HomePage from "./pages/HomePage/HomePage.jsx";
-import Login from "./pages/Login/Login";
-import Nav from "./component/Nav/Nav";
-import Cart from "./pages/Cart/Cart";
+import { Routes, Route } from 'react-router-dom';
+import './App.scss';
+import HomePage from './pages/HomePage/HomePage.jsx';
+import Login from './pages/Login/Login';
+import Nav from './component/Nav/Nav';
+import Cart from './pages/Cart/Cart';
+import { useSelector } from 'react-redux';
+import { selectDarkMode } from './store/selectors/darkModeSelectors';
 function App() {
+  const darkMode = useSelector(selectDarkMode);
+
+  const darkModeStyle = {
+    backGroundColor: '#333',
+    color: 'white',
+  };
+
+  const lightModeStyle = {
+    backGroundColor: 'white',
+    color: '#333',
+  };
+
   return (
     <>
-      <main>
+      <main style={darkMode ? darkModeStyle : lightModeStyle}>
         <header>
           <Nav />
         </header>
         <div>
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route path='/' element={<HomePage />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/cart' element={<Cart />} />
           </Routes>
         </div>
       </main>
