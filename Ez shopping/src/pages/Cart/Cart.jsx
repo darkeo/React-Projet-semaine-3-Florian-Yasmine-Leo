75d2a64f6ed21bf2dfa009ea85d78bbce6a9f55f
mineyas <mineyaskha@gmail.com>
2023-10-06 09:19:29 +0100

    added darkmode

--- Ez shopping/src/pages/Cart/Cart.jsx
+++ Ez shopping/src/pages/Cart/Cart.jsx
@@ -1,21 +1,24 @@
-import { useDispatch, useSelector } from 'react-redux';
-import CartList from '../../component/CartList/CartList';
-import './Cart.scss';
-import { removeAllItems } from '../../store/slices/cartSlice';
-import { selectCart } from '../../store/selectors/cartSelectors';
-import { selectUser } from '../../store/selectors/userSelectors';
+import { useDispatch, useSelector } from "react-redux";
+import CartList from "../../component/CartList/CartList";
+import "./Cart.scss";
+import { removeAllItems } from "../../store/slices/cartSlice";
+import { selectCart } from "../../store/selectors/cartSelectors";
+import { selectUser } from "../../store/selectors/userSelectors";
+import { selectDarkMode } from "../../store/selectors/darkModeSelectors";
 import {
   getFromLocalStorage,
   isKeyInLocalStorage,
-} from '../../utils/localStorage';
+} from "../../utils/localStorage";
 
 export default function Cart() {
   const dispatch = useDispatch();
   const cart = useSelector(selectCart);
   const userSelector = useSelector(selectUser);
+  const darkMode = useSelector(selectDarkMode);
+
   const user = () => {
-    return isKeyInLocalStorage('user')
-      ? getFromLocalStorage('user')
+    return isKeyInLocalStorage("user")
+      ? getFromLocalStorage("user")
       : userSelector;
   };
   console.log(user);
@@ -24,27 +27,36 @@ export default function Cart() {
     dispatch(removeAllItems());
   };
   return (
-    <div style={{ minHeight: '100vh' }}>
-      {cart.length > 0 ? (
-        <h1>
-          {user().firstName.length > 0
-            ? `${user().firstName},`
-            : `${user().firstName}`}{' '}
-          voici votre panier
-        </h1>
-      ) : (
-        <h1>{`${user().firstName},`} votre Panier est vide</h1>
-      )}
-      {cart.length > 0 && (
-        <button
-          onClick={() => {
-            handleClick();
-          }}
-        >
-          Vider le panier
-        </button>
-      )}
+    <div style={{ minHeight: "100vh" }} className="cart-parent">
+      <div className="cart-title-and-button-box">
+        {cart.length > 0 ? (
+          <h1>
+            {user().firstName.length > 0
+              ? `${user().firstName},`
+              : `${user().firstName}`}{" "}
+            voici votre panier
+          </h1>
+        ) : (
+          <h1>{`${user().firstName},`} votre Panier est vide</h1>
+        )}
 
+        {cart.length > 0 && (
+          <div className="cart-button-remove-all">
+            <button
+              className={
+                darkMode
+                  ? "dark-rem-all cart-remove-all-button"
+                  : "light-rem-all cart-remove-all-button"
+              }
+              onClick={() => {
+                handleClick();
+              }}
+            >
+              Vider le panier
+            </button>
+          </div>
+        )}
+      </div>
       <CartList />
     </div>
   );
